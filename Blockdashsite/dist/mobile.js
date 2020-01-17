3e844f775a2b810f8a6ca41df387840e25ff2ec9
Ian-mccallum <59983652+Ian-mccallum@users.noreply.github.com>
2020-01-16 20:02:25 -0600

    Add files via upload

---
+++ Blockdashsite/dist/mobile.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mobile__menu").style.width = "100%";
+    document.getElementById("main_menu").style.width = "0%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mobile__menu").style.width = "0";
+  }
+  
